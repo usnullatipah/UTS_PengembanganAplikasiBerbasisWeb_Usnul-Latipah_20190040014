7b8fc9ea1f87c29c94e8457498f89546ff9daa21
Usnul Latipah <58874864+usnullatipah@users.noreply.github.com>
2021-12-26 21:53:21 +0700

    Update config.php

--- CRUD/config.php
+++ CRUD/config.php
@@ -1,6 +1,6 @@
 <?php
 //koneksi ke database mysql,
-$koneksi = mysqli_connect("localhost","root","","smk_suryakencan");
+$koneksi = mysqli_connect("localhost","root","","smk_suryakencana");
 
 //cek jika koneksi ke mysql gagal, maka akan tampil pesan berikut
 if (mysqli_connect_errno()){
